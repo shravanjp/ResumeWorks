d64143c18c3ec8c1f206a7d9198d0b5b58605ca2
ShravanJP <shravanjp8910@gmail.com>
2023-05-05 21:04:13 +0530

    add : include urls for dashboard, to list coding skill and to add coding skill

--- resume_works/resume_generator/urls.py
+++ resume_works/resume_generator/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name = 'home'),
+    path('', views.home, name = 'home'),
+    path('dashboard/', views.dashboard, name = 'dashboard'),
+    # path('projects/', views.project_list, name='project_list'),
+    # path('projects/<int:pk>/', views.project_detail, name='project_detail'),
+    # path('employees/', views.employee_list, name='employee_list'),
+    # path('employees/<int:pk>/', views.employee_detail, name='employee_detail'),
+    path('coding_skills/', views.skills_list, name='skills_list'),
+    path('coding_skills/add_skill/', views.add_skill, name = 'add_skill'),
+    # path('tools/', views.tool_list, name='tool_list'),
+    # path('generate-resume/<int:pk>/', views.generate_resume, name='generate_resume'),
+
 ]
