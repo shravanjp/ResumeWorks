b6231ac02bdd40ee2ff24e678cf42a01d7d40d32
ShravanJP <shravanjp8910@gmail.com>
2023-05-06 01:37:51 +0530

    implement delete_coding_skill view function
    Here the id is sent using GET request

--- resume_works/resume_generator/urls.py
+++ resume_works/resume_generator/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     # path('projects/<int:pk>/', views.project_detail, name='project_detail'),
     # path('employees/', views.employee_list, name='employee_list'),
     # path('employees/<int:pk>/', views.employee_detail, name='employee_detail'),
-    path('coding_skills/', views.coding_skills_list, name='skills_list'),
+    path('coding_skills/', views.coding_skills_list, name='coding_skills_list'),
     path('coding_skills/create_coding_skill/', views.create_coding_skill, name = 'create_coding_skill'),
     # path('coding_skills/edit_skill/', views.create_skill, name = 'create_skill'),
-    # path('coding_skills/delete_coding_skill/', views.delete_coding_skill, name = 'delete_coding_skill'),
+    path('coding_skills/delete_coding_skill/<int:coding_skill_id>/', views.delete_coding_skill, name = 'delete_coding_skill'),
     # path('tools/', views.tool_list, name='tool_list'),
     # path('generate-resume/<int:pk>/', views.generate_resume, name='generate_resume'),
 
--- resume_works/resume_generator/views.py
+++ resume_works/resume_generator/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import *
@@ -14,19 +14,19 @@ def dashboard(request):
 
 @login_required
 def coding_skills_list(request):
-    coding_skills = CodingSkill.objects.all()
+    coding_skills = CodingSkill.objects.filter(is_deleted=False)
     return render(request,'resume_generator/coding_skills_list.html',{'coding_skills':coding_skills})
 
 @login_required
 def create_coding_skill(request):
-    coding_skills = CodingSkill.objects.all()
+    coding_skills = CodingSkill.objects.filter(is_deleted=False)
     if request.method == 'POST':
         form = CodingSkillForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
             name = ("").join([x.lower() for x in name]) 
 
-            if not CodingSkill.objects.filter(name=name).exists():
+            if not CodingSkill.objects.filter(is_deleted=False, name=name).exists():
                 coding_skill_model = CodingSkill(name=name)
                 coding_skill_model.save()
                 messages.success(request, 'Skill added successfully!')
@@ -52,6 +52,11 @@ def create_coding_skill(request):
         return render(request, 'resume_generator/create_coding_skill.html', context)
 
 
-# @login_required
-# def delete_skill(request):
+@login_required
+def delete_coding_skill(request, coding_skill_id):
+    coding_skill = get_object_or_404(CodingSkill, id=coding_skill_id)
+    coding_skill.is_deleted = True
+    coding_skill.save()
+    return redirect('coding_skills_list')
+
     
