008438df3e23886e8f8a66996eac38bfdbc6bd45
ShravanJP <shravanjp8910@gmail.com>
2023-05-06 00:21:23 +0530

    change url path add_skill->create_skill, and other changes
    this also includes the changes due to change in model name from Skill to CodingSkills and form name from SkillForm to CodingSkillForm

--- resume_works/resume_generator/views.py
+++ resume_works/resume_generator/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import *
-from .forms import SkillForm
+from .forms import CodingSkillForm
 # Create your views here.
 @login_required(login_url='accounts/login/')
 def home(request):
@@ -14,25 +14,25 @@ def dashboard(request):
 
 @login_required
 def skills_list(request):
-    skills = Skill.objects.all()
+    skills = CodingSkills.objects.all()
     return render(request,'resume_generator/skills_list.html',{'skills':skills})
 
 @login_required
-def add_skill(request):
-    skills = Skill.objects.all()
+def create_skill(request):
+    skills = CodingSkills.objects.all()
     if request.method == 'POST':
-        form = SkillForm(request.POST)
+        form = CodingSkillForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
             name = ("").join([x.lower() for x in name]) 
 
-            if not Skill.objects.filter(name=name).exists():
-                skill_model = Skill(name=name)
+            if not CodingSkills.objects.filter(name=name).exists():
+                skill_model = CodingSkills(name=name)
                 skill_model.save()
                 messages.success(request, 'Skill added successfully!')
             else:
                 messages.error(request, 'Skill already exists!')
-            return redirect('add_skill')
+            return redirect('create_skill')
 
         else:
             messages.info(request, form.errors)
@@ -41,7 +41,7 @@ def add_skill(request):
             #     'form': form,
             #     'skills': skills
             # }
-            return render(request, 'resume_generator/add_skill.html', {'form':form})
+            return render(request, 'resume_generator/create_skill.html', {'form':form})
     else:
         print("2nd part")
         form = SkillForm()
@@ -49,4 +49,6 @@ def add_skill(request):
             'form': form,
             'skills': skills
         }
-        return render(request, 'resume_generator/add_skill.html', context)
+        return render(request, 'resume_generator/create_skill.html', context)
+
+
