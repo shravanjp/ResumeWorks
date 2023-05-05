ad730ab6a8fee8ca0046fd487e83eb1fe61fd667
ShravanJP <shravanjp8910@gmail.com>
2023-05-06 00:23:54 +0530

    change model name from Skill to CodingSkills and Tool to Tools

--- resume_works/resume_generator/admin.py
+++ resume_works/resume_generator/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Skill)
-admin.site.register(Tool)
+admin.site.register(CodingSkills)
+admin.site.register(Tools)
 # Register your models here.
--- resume_works/resume_generator/models.py
+++ resume_works/resume_generator/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Tool(models.Model):
+class Tools(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -11,7 +11,7 @@ class Tool(models.Model):
     def __str__(self):
         return self.name
 
-class Skill(models.Model):
+class CodingSkills(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
