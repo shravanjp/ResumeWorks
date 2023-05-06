6edb553c71c22bfe6c9265ec2a5be95b8a1975eb
ShravanJP <shravanjp8910@gmail.com>
2023-05-06 23:50:05 +0530

    fetch real data for the dashboard

--- resume_works/resume_generator/views/dashboard_view.py
+++ resume_works/resume_generator/views/dashboard_view.py
@@ -11,8 +11,12 @@ def home_view(request):
 def dashboard_view(request):
     coding_skills_len = len(CodingSkill.objects.filter(is_deleted=False))
     tools_len = len(Tool.objects.filter(is_deleted=False))
+    projects_len = 0
+    employees_len = 0
     context = {
         'coding_skills_len': coding_skills_len,
-        'tools_len': tools_len
+        'tools_len': tools_len,
+        'projects_len' : projects_len,
+        'employees_len' : employees_len,
     }
     return render(request,'resume_generator/dashboard.html',context)
